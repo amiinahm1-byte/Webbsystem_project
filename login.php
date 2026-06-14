3959231a1c627c1ae596a48b8d18be7d374c9378
SalamancaTok <jacob20tokhi@gmail.com>
2026-06-14 16:49:10 +0200

    comments
    Added comments to improve code readability.

--- login.php
+++ login.php
@@ -1,27 +1,30 @@
 <?php
-
+//connects to local database
 $mysqli = new mysqli("localhost", "pi_user", "skola123", "security_db");
-
+//if error
 if ($mysqli->connect_errno) {
     echo "Couldn't connect: " . $mysqli->connect_error;
     exit();
 }
 
+//get oseudo and password
 $pseudo = $_POST['pseudo']; 
 $password = $_POST['password'];
 
-
+//query for matching rows credentials
 $query_str = "SELECT * FROM users WHERE pseudo='$pseudo' AND password='$password'";
-$result = $mysqli->query($query_str);
+$result = $mysqli->query($query_str);//execute quesry inside the MySQL engine
+
 
 if ($result->num_rows > 0) {
-    session_start();
+    session_start(); //start session
     $row = $result->fetch_assoc(); 
     
+    //creates a secure server session tracking pseudo and role
     $_SESSION['pseudo'] = $pseudo;
     $_SESSION['role'] = $row['role']; 
     
-    
+    //sends a raw HTTP header
     header("Location: home.php"); 
     exit();
 } else {
